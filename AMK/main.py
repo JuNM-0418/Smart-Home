e75619b37c3e7d41ead45864e98d47c7c7afbef7
PenLoo98 <76810066+PenLoo98@users.noreply.github.com>
2022-12-09 17:45:00 +0900

    update files 2022.12.9

--- AMK/main.py
+++ AMK/main.py
@@ -1,21 +1,25 @@
-from modules.audio_handler import TTS, STT
+from modules.audio_handler import TTS,STT
 from modules.youtube import YoutubePlayer
+from modules.schedulelr import Alarming
+
+
 
 def main():
     stt = STT()
     tts = TTS()
     youtube = YoutubePlayer()
+
     while True:
-        recog = stt.get_text(timeout=2, phrase=2)
+        recog = stt.get_text(timeout=5, phrase=5)
         if not recog in ["지니야", "기가지니"]:
             continue
         print("start command mode")
         if youtube.is_set():
             youtube.pause()
         tts.direct_tts("네")
-        command = stt.get_text(timeout=2, phrase=3)
+        command = stt.get_text(timeout=5, phrase=5)
         if command is None:
-            tts.direct_tts("잘 못알아들었어요")
+            tts.direct_tts("잘 못알17아들었어요")
             continue
         print(f"인식된 텍스트: {command}")
         if "유튜브" in command:
@@ -27,9 +31,11 @@ def main():
             elif "중지" in command or "꺼" in command:
                 tts.direct_tts("오디오를 종료합니다.")
                 youtube.stop()
-                
+        elif "알람" in command:
+            Alarming().set_alarm()
         if youtube.is_set():
             youtube.play()
+                
                 
 if __name__ == "__main__":
     main()
